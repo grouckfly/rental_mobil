3aef295c5735c1a657f92f85e0ea15aad1a4bf4f
grouckfly <grouckfly@gmail.com>
2025-07-13 14:48:21 +0700

    Pembuatan Dashboard admin

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,16 @@
 // File: includes/footer.php
 
 // Mendefinisikan base URL lagi untuk konsistensi jika footer dipanggil terpisah
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
-$host = $_SERVER['HTTP_HOST'];
-$script_name = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$base_url = $protocol . "://" . $host . str_replace('/includes', '', $script_name);
+// Ini memastikan $base_url selalu tersedia
+if (!isset($base_url)) {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+    $host = $_SERVER['HTTP_HOST'];
+    $project_path = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+    if (strpos($_SERVER['REQUEST_URI'], '/admin/') !== false || strpos($_SERVER['REQUEST_URI'], '/karyawan/') !== false || strpos($_SERVER['REQUEST_URI'], '/pelanggan/') !== false) {
+        $project_path = dirname($project_path) . '/';
+    }
+    $base_url = $protocol . "://" . $host . $project_path;
+}
 ?>
 
             </div> </main> </div> <footer class="main-footer">
@@ -16,6 +22,17 @@ $base_url = $protocol . "://" . $host . str_replace('/includes', '', $script_nam
 
     <script src="<?= $base_url ?>assets/js/script.js"></script>
     <script src="<?= $base_url ?>assets/js/dark-mode.js"></script>
+
+    <?php
+    // Memuat JS spesifik role jika ada
+    $current_dir = basename(dirname($_SERVER['PHP_SELF']));
+    if (in_array($current_dir, ['admin', 'karyawan', 'pelanggan'])) {
+        $js_file_path = "js/{$current_dir}.js";
+        if (file_exists($js_file_path)) {
+            echo "<script src=\"{$js_file_path}\"></script>";
+        }
+    }
+    ?>
 
 </body>
 </html>
